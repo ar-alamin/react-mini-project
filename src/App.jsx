030cccc75e64e7cb787173777be0e9220c210420
Abdur Rahman Al Amin <95558975+ar-alamin@users.noreply.github.com>
2023-12-14 03:25:25 +0000

    add to cart project in redux toolkit

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,20 @@
-import CounterPage from "./pages/CounterPage";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Cart from "./pages/Cart";
+import Navbar from "./components/Navbar";
 
 const App = () => {
   return (
-    <>
-      <CounterPage />
-    </>
+    <div className="App"> 
+        <BrowserRouter>
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<Home />}></Route>
+            <Route path="/cart" element={<Cart />}></Route>
+          </Routes>
+        </BrowserRouter>
+     
+    </div>
   );
 };
 
---
+++ src/pages/Home.jsx
@@ -0,0 +1,17 @@
+import Products from "../components/Products";
+
+const Home = () => {
+  return (
+    <div>
+      <h2 className="heading">Welocome to the Redux toolkit</h2>
+
+      <section>
+        <h3>Products</h3>
+
+        <Products />
+      </section>
+    </div>
+  );
+};
+
+export default Home;
